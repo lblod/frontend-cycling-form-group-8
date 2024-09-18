b552f1b53505648bf34a3fbbb444901f000d5c76
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-09-18 12:20:50 +0200

    Fetch municipalities from gpx services

--- app/components/rdf-form-fields/pelotonplanner/gemeenten-prijzen.js
+++ app/components/rdf-form-fields/pelotonplanner/gemeenten-prijzen.js
@@ -1,4 +1,15 @@
-import Component from '@glimmer/component';
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+
 export default class GemeentenPrijzen extends Component {
-  
+  @action
+  async fetchMunicipalities() {
+    const fileId = "66e9eed8a09931000d000006"; // TODO: should not be hardcoded
+
+    const response = await fetch(`/gpx/municipalities?id=${fileId}`, {
+      method: "GET",
+    });
+    const body = await response.json();
+    console.log(body);
+  }
 }
